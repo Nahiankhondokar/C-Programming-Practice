20d7488f05bfdc4ec4bce0cf119a6ad28badedc7
nahiankhondokar <info.nahian13@gmail.com>
2025-04-09 17:00:26 +0600

    for loop sum

--- index.c
+++ index.c
@@ -1,13 +1,26 @@
 #include<stdio.h>
 
+// int main ()
+// {
+//    int numb;
+//    scanf("%d", &numb);
+//    for (int i = 1; i < numb; i++)
+//    {
+//       printf("%d \n", i);
+//    }
+
+//    return 0;
+// }
+
 int main ()
 {
-   int numb;
-   scanf("%d", &numb);
-   for (int i = 1; i < numb; i++)
+   int n;
+   scanf("%d", &n);
+   int sum = 0;
+   for (int i = 1; i <= n; i++)
    {
-      printf("%d \n", i);
+      sum += i;
    }
-
+   printf("%d \n", sum);
    return 0;
 }
