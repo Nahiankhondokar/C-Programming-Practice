178b420e501c9d791ec43c77ca303b2897220960
nahiankhondokar <info.nahian13@gmail.com>
2025-04-17 13:40:42 +0600

    multiplication

--- F_Multiplication_table.c
+++ F_Multiplication_table.c
@@ -5,7 +5,7 @@ int main ()
     scanf("%d", &a);
     for (int i = 1; i <= 12; i++)
     {
-        printf("%d \n", a + "*" + i + a*i);
+        printf("%d * %d = %d \n", a, i, a*i);
     }
     
     return 0;
