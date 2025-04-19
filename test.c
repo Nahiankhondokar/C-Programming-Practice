6b9a52d3f6f6a810bd13b178e2f6430be8412ef2
nahiankhondokar <info.nahian13@gmail.com>
2025-04-19 16:44:35 +0600

    Revert "revert commit"
    This reverts commit ab481ac181b922bb71b90dec987e26517a83acfb.

--- test.c
+++ test.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
 int main ()
 {
-    printf("This is test commit");
+    int n;
+    scanf("%d", &n);
+    int arr[n];
 
-    for (size_t i = 0; i < 10; i++)
+    for (int i = 0; i < n; i++)
     {
-        /* code */
+        scanf("%d", &arr[i]);
     }
     
+    for (int j = 2; j >= 0; j--)
+    {
+        printf("%d ", arr[j]);
+    }
+
+    printf("This is test commit");
     return 0;
 }
