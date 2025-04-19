a3b615f534dde1ea6b54d143b700de3f71d36cf1
nahiankhondokar <info.nahian13@gmail.com>
2025-04-19 16:34:44 +0600

    new array commit

--- test.c
+++ test.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
 int main ()
 {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    
+    for (int j = 2; j >= 0; j--)
+    {
+        printf("%d ", arr[j]);
+    }
+
     printf("This is test commit");
     return 0;
 }
