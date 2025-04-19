8f8f2960ee5f7d71d60677b8c320b0cd738d35b4
nahiankhondokar <nahiankhondokarphp@gmail.com>
2025-04-20 00:04:02 +0600

    loop

--- index.c
+++ index.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main ()
 {
+    int n;
+    scanf("%d", &n);
+    if(n > 0){
+        for(int i = 1; i <= n; i++){
+            printf("%d. I Want More Assignments\n", i);
+        }
+    }
     
     return 0;
 }
