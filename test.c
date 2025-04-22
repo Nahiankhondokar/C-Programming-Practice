8a2b41cc37d8036c2cf561f02f167f645ce6807a
nahiankhondokar <info.nahian13@gmail.com>
2025-04-22 18:06:46 +0600

    Revert "Merge pull request #10 from Nahiankhondokar/staging"
    This reverts commit 20ed267b6cc0d54b3fc447e0f01944f05a713e74, reversing
    changes made to b4a4ba30e8771a6ca448f665b16df7c1d6dd62cf.

--- test.c
+++ test.c
@@ -5,8 +5,7 @@ int main ()
     printf("This is test commit");
     printf("This is test commit");
     printf("This is test commit");
-    printf("This is test PR 2nd");
+
     printf("This is test PR 1st");
-    printf("This is test PR 2nd");
     return 0;
 }
