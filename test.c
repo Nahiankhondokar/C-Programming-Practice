5a65862b0631a9bb95a4b039618beab349b5d8b6
nahiankhondokar <info.nahian13@gmail.com>
2025-04-22 16:48:57 +0600

    pr 2

--- test.c
+++ test.c
@@ -5,7 +5,8 @@ int main ()
     printf("This is test commit");
     printf("This is test commit");
     printf("This is test commit");
-
+    printf("This is test PR 2nd");
     printf("This is test PR 1st");
+    printf("This is test PR 2nd");
     return 0;
 }
