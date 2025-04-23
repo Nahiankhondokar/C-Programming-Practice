ca501f3599a13e112ac1eade2e722ad85cf06027
nahiankhondokar <info.nahian13@gmail.com>
2025-04-23 11:57:36 +0600

    string array

---
+++ string_array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    // char s[20];
+    // scanf("%s", &s); // can't take space string
+    // gets(s);
+    // fgets(s, 20, stdin); // count enter also
+
+    // direct intialization
+    char s[15] = "hello world";
+    printf("%s", s);
+
+    return 0;
+}
