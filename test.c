3d50d07e1ba3b58f8ffee493806323830f034be7
nahiankhondokar <info.nahian13@gmail.com>
2025-04-27 18:31:28 +0600

    Revert "Merge pull request #13 from Nahiankhondokar/staging"
    This reverts commit 2bd8d7637dcf8cd15d99cf5c436b638e7dfd819a, reversing
    changes made to c2d95ab85213ad67a86045d1d62130e6c807e65a.

--- test.c
+++ test.c
@@ -7,8 +7,5 @@ int main ()
 
     printf("This is another staging commit 02");
 
-    printf("This is another staging commit 04");
-    printf("This is another staging commit 05");
-
     return 0;
 }
