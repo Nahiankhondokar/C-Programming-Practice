4886268081b65651f5412dabd3ca5d66b08e184b
nahiankhondokar <nahiankhondokarphp@gmail.com>
2025-04-29 10:31:28 +0600

    wip

--- Frequency-Array.c
+++ Frequency-Array.c
@@ -8,15 +8,25 @@ int main ()
     {
         scanf("%d", &arr[i]);
     }
-    int freq[100] = {0};
-    int x = 0, y=n-1;
+    int freq[5] = {0};
+    // int x = 0, y=n-1;
     for (int i = 0; i < n; i++)
     {
-        if(arr[x] == arr[y]){
-            freq[i] = {arr[x] : "0"};
+        if(arr[i] == 0){
+            freq[0]++;
+        }else if(arr[i] == 1){
+            freq[1]++;
+        }else if(arr[i] == 2){
+            freq[2]++;
+        }else if(arr[i] == 3){
+            freq[3]++;
         }
     }
     
+    printf("0 -> %d\n", freq[0]);
+    printf("1 -> %d\n", freq[1]);
+    printf("2 -> %d\n", freq[2]);
+    printf("3 -> %d\n", freq[3]);
     
 
     return 0;
