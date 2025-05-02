681879ccc2a97a91dc309ea8491181fd7acf6639
nahiankhondokar <nahiankhondokarphp@gmail.com>
2025-05-02 19:42:49 +0600

    frequent array count

--- Frequency-Array.c
+++ Frequency-Array.c
@@ -36,40 +36,7 @@
 // }
 
 
-int main ()
-{
-    int n;
-    scanf("%d", &n);
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-    int freq[5] = {0};
-    for (int i = 0; i < n; i++)
-    {
-        if(arr[i] == 0){
-            freq[0]++;
-        }else if(arr[i] == 1){
-            freq[1]++;
-        }else if(arr[i] == 2){
-            freq[2]++;
-        }else if(arr[i] == 3){
-            freq[3]++;
-        }
-    }
-    
-    printf("0 -> %d\n", freq[0]);
-    printf("1 -> %d\n", freq[1]);
-    printf("2 -> %d\n", freq[2]);
-    printf("3 -> %d\n", freq[3]);
-    
-
-    return 0;
-}
-
-
-// int main2 ()
+// int main ()
 // {
 //     int n;
 //     scanf("%d", &n);
@@ -79,16 +46,51 @@ int main ()
 //         scanf("%d", &arr[i]);
 //     }
 
-//     int freq[4] = {0};
-//     for (int i = 0; i < 5; i++)
+//     int freq[5] = {0};
+//     for (int i = 0; i < n; i++)
 //     {
-//         freq[arr[i]]++;
+//         if(arr[i] == 0){
+//             freq[0]++;
+//         }else if(arr[i] == 1){
+//             freq[1]++;
+//         }else if(arr[i] == 2){
+//             freq[2]++;
+//         }else if(arr[i] == 3){
+//             freq[3]++;
+//         }
 //     }
-
-//     for (int i = 0; i < 5; i++)
-//     {
-//         printf("0 -> %d\n", freq[0]);
-//     }
+    
+//     printf("0 -> %d\n", freq[0]);
+//     printf("1 -> %d\n", freq[1]);
+//     printf("2 -> %d\n", freq[2]);
+//     printf("3 -> %d\n", freq[3]);
+    
 
 //     return 0;
 // }
+
+
+int main ()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int freq[5] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        int val = arr[i];
+        freq[val]++;
+    }
+
+    for (int i = 0; i <= 5; i++)
+    {
+        printf("%d -> %d\n",i, freq[i]);
+    }
+
+    return 0;
+}
