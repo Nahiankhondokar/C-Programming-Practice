dac4a9c1de314a524093acc004dd0d42f843782b
nahiankhondokar <info.nahian13@gmail.com>
2025-05-07 16:58:56 +0600

    pattern 06

--- Module 13/pattern.c
+++ Module 13/pattern.c
@@ -73,29 +73,59 @@
 
 
 // Pattern 04
-int main ()
-{
-    int n;
-    scanf("%d", &n);
-    int star = 1;
-    int space = n-1;
+// int main ()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int star = 1;
+//     int space = n-1;
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int k = 0; k < space; k++)
-        {
-            printf(" ");
-        }
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int k = 0; k < space; k++)
+//         {
+//             printf(" ");
+//         }
         
-        for (int j = 0; j < star; j++)
-        {
-            printf("*");
-        }
+//         for (int j = 0; j < star; j++)
+//         {
+//             printf("*");
+//         }
         
-        printf("\n");
-        star++;
-        space--;
-    }
+//         printf("\n");
+//         star++;
+//         space--;
+//     }
     
-    return 0;
-}
+//     return 0;
+// }
+
+
+// Pattern 05
+// int main ()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int space = n-1;
+//     int count = 1;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int k = 0; k < space; k++)
+//         {
+//             printf(" ");
+//         }
+        
+//         for (int j = 1; j <= count; j++)
+//         {
+//             printf("%d ", j);
+//             // printf("%d", j);
+//         }
+        
+//         printf("\n");
+//         count++;
+//         space--;
+//     }
+    
+//     return 0;
+// }
