b5a64d959986bd31a18787e42e7306f4acdf3925
nahiankhondokar <info.nahian13@gmail.com>
2025-05-07 12:44:54 +0600

    module 13, Patter 01

---
+++ Module 13/pattern.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+// Patter 01
+// int main ()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int star = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < star ; j++)
+//         {
+//             printf("*");
+//         }
+        
+//         printf("\n");
+//         star++;
+//     }
+    
+//     return 0;
+// }
+
