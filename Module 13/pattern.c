8d0eafea7ac228877fd7db445214b28ca5e8cfc3
nahiankhondokar <info.nahian13@gmail.com>
2025-05-07 13:41:40 +0600

    patter 02

--- Module 13/pattern.c
+++ Module 13/pattern.c
@@ -20,3 +20,24 @@
 //     return 0;
 // }
 
+
+// Patter 02
+int main ()
+{
+    int n;
+    scanf("%d", &n);
+    int star = n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < star; j++)
+        {
+            printf("*");
+        }
+        
+        printf("\n");
+        star--;
+    }
+    
+    return 0;
+}
