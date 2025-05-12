e7f2f17b53211f537a9356d1a189aad4652365e2
nahiankhondokar <info.nahian13@gmail.com>
2025-05-12 16:28:59 +0600

    function module

---
+++ Module 14/function.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void sum (int num1, int num2)
+{
+    int ans = num1 + num2;
+    return;
+    printf("%d", ans);
+}
+
+int main ()
+{
+    sum(10, 20);
+    printf("hello");
+    return 0;
+}
