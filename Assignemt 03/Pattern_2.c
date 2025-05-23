7e3be73beeacffff33d009761fe3ffbcb1b709aa
nahiankhondokar <nahiankhondokarphp@gmail.com>
2025-05-23 14:27:58 +0600

    number pattern solve

--- Assignemt 03/Pattern_2.c
+++ Assignemt 03/Pattern_2.c
@@ -5,7 +5,16 @@ int main ()
     scanf("%d", &n);
     for (int i = 0; i < n; i++)
     {
+        for (int k = i; k < n-1; k++)
+        {
+            printf(" ");
+        }
         
+        for (int j = i+1; j > 0; j--)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
     }
     
     return 0;
