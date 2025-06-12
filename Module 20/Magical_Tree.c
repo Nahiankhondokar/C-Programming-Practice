addfb28cd3b5d3f085946ec011da97c00316ebd2
nahiankhondokar <nahiankhondokarphp@gmail.com>
2025-06-12 11:59:01 +0600

    up-arrow pattern design

---
+++ Module 20/Magical_Tree.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main ()
+{
+    int n;
+    scanf("%d", &n);
+    int stars = 6;
+    if(n > 1){
+        int newStarts = (n - 1)/2;
+        stars += newStarts;
+    }
+    
+    // pyramid
+    int inc = 1;
+    for (int i = 0; i < stars; i++)
+    {
+        for (int k = 1; k < stars-i; k++)
+        {
+            printf(" ");
+        }
+        
+        for (int j = 0; j < inc; j++)
+        {
+            printf("*");
+        }
+        inc += 2;
+        printf("\n");
+    }
+
+    // bottom stricks
+    int r = 5;
+    for (int m = 0; m < r; m++)
+    {
+        for (int k = 0; k < r; k++)
+        {
+            printf(" ");
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
