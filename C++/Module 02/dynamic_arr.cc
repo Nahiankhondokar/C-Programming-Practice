9da17ee48ec16761a71988c53e0f3ac3b64c307b
nahiankhondokar <nahiankhondokarphp@gmail.com>
2025-06-28 21:12:43 +0600

    dynamic array declare

---
+++ C++/Module 02/dynamic_arr.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h> // This is for all c++ headers
+using namespace std; // for std globally
+
+int main()
+{
+    int *p = new int[5]; // dynamic array
+    for (int i = 0; i < 5; i++)
+    {
+        cout << i << " ";
+    }
+    
+    return 0;
+}
