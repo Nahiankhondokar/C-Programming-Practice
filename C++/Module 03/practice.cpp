0da283a7e443dd3eade234c39b2b83866e7a3d12
nahiankhondokar <nahiankhondokarphp@gmail.com>
2025-08-01 09:57:41 +0600

    palindrom problem solve by c++

--- C++/Module 03/practice.cpp
+++ C++/Module 03/practice.cpp
@@ -1,46 +1,87 @@
 #include<bits/stdc++.h> 
 using namespace std; 
 
+/**
+ * 5
+ * 1 3 2 3 1
+ * Palindrome array
+ */
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n]; 
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int x = 0;
+    int y = 0;
+    int flag = 1;
+    for (int j = 0; j < n; j++)
+    {
+        x = arr[j];
+        for (int k = n-j; k > j; k--)
+        {
+            y = arr[k - 1];
+            if(x != y){
+                flag = 0;
+                break;
+            }else if(x == y ){
+                break;
+            }
+        }
+    }
+
+    if(flag == 1){
+        cout << "YES";
+    }else {
+         cout << "NO";
+    }
+
+    return 0;
+}
 
 /**
  * 5 + 10 = 15
  * Print yes or right answer
  */
-int main()
-{
-    int a;
-    char s;
-    int b;
-    char q;
-    int c;
+// int main()
+// {
+//     int a;
+//     char s;
+//     int b;
+//     char q;
+//     int c;
 
-    cin >> a >> s >> b >> q >> c;
+//     cin >> a >> s >> b >> q >> c;
 
-    if(s == '+'){
-        int result = a + b;
-         if(c == result){
-            cout << "Yes";
-        }else {
-            cout << result;
-        }
-    }else if(s == '-'){
-        int result = a - b;
-         if(c == result){
-            cout << "Yes";
-        }else {
-            cout << result;
-        }
-    }else if(s == '*'){
-        int result = a * b;
-        if(c == result){
-            cout << "Yes";
-        }else {
-            cout << result;
-        }
-    }
+//     if(s == '+'){
+//         int result = a + b;
+//          if(c == result){
+//             cout << "Yes";
+//         }else {
+//             cout << result;
+//         }
+//     }else if(s == '-'){
+//         int result = a - b;
+//          if(c == result){
+//             cout << "Yes";
+//         }else {
+//             cout << result;
+//         }
+//     }else if(s == '*'){
+//         int result = a * b;
+//         if(c == result){
+//             cout << "Yes";
+//         }else {
+//             cout << result;
+//         }
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 
 
 /**
