1338acb0075280b699dee18ec3516419607f7a7e
nahiankhondokar <nahiankhondokarphp@gmail.com>
2025-08-01 11:24:52 +0600

    test-case wise smallets pair find

--- C++/Module 03/practice.cpp
+++ C++/Module 03/practice.cpp
@@ -2,46 +2,89 @@
 using namespace std; 
 
 /**
+* 1 -> test case
+* 4 -> array number
+* 20 1 9 4 -> array value
+
+ * arr[0]+arr[1]+b-a = sum
+ *  1<=b<a<=N
+ */
+int main ()
+{
+    int t;
+    cin >> t;
+
+    for (int z = 0; z < t; z++){
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++){
+            cin >> arr[i];
+        }
+
+        int a = 0;
+        int b = 0;
+
+        int result = INT_MAX;
+            for (int j = 0; j < n; j++){
+                for (int k = j+1; k < n; k++){
+                    a = j+1;
+                    b = k+1;
+                    // cout << arr[j] << " " << arr[k] << " " << b << " " << a << endl;
+                    int cal = arr[j]+arr[k]+b-a;
+                    if(result > cal){
+                        result = cal;
+                    }
+                }
+        }
+
+        cout << result << endl;
+    }
+    
+    return 0;
+}
+
+/**
  * 5
  * 1 3 2 3 1
  * Palindrome array
  */
-int main()
-{
-    int n;
-    cin >> n;
-    int arr[n]; 
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n]; 
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
 
-    int x = 0;
-    int y = 0;
-    int flag = 1;
-    for (int j = 0; j < n; j++)
-    {
-        x = arr[j];
-        for (int k = n-j; k > j; k--)
-        {
-            y = arr[k - 1];
-            if(x != y){
-                flag = 0;
-                break;
-            }else if(x == y ){
-                break;
-            }
-        }
-    }
+//     int x = 0;
+//     int y = 0;
+//     int flag = 1;
+//     for (int j = 0; j < n; j++)
+//     {
+//         x = arr[j];
+//         for (int k = n-j; k > j; k--)
+//         {
+//             y = arr[k - 1];
+//             if(x != y){
+//                 flag = 0;
+//                 break;
+//             }else if(x == y ){
+//                 break;
+//             }
+//         }
+//     }
 
-    if(flag == 1){
-        cout << "YES";
-    }else {
-         cout << "NO";
-    }
+//     if(flag == 1){
+//         cout << "YES";
+//     }else {
+//          cout << "NO";
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 
 /**
  * 5 + 10 = 15
